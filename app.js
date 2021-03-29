f981516370c22b57f93f46e40858acf4f4805b47
Joao <jp_bast@hotmail.com>
2021-03-29 16:45:58 -0300

    removing template engine functions

--- app.js
+++ app.js
@@ -13,8 +13,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/views')));
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'));
 app.use('/', userRouter);
 app.use('/mural', muralRouter);
 
